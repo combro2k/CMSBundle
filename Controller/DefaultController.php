1bc193adae22432c6dad3a815e0bad4c2de42fa3
Martijn van Maurik <martijn.van.maurik@vitaphone.nl>
2013-01-10 14:55:01 +0100

    testing pdf

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace CMS\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use WowApi\Client;
 use WowApi\Request\Curl;
