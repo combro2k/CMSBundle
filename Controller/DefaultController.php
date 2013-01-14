c3e00bcad9ff0e6e257977339d18ceab8c15adaa
Martijn van Maurik <martijn.van.maurik@vitaphone.nl>
2013-01-14 11:15:37 +0100

    Test pdf

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -16,4 +16,13 @@ class DefaultController extends Controller
     {
         return array('name' => $name);
     }
+
+    /**
+     * @Route("/pdf")
+     * @Template()
+     */
+    public function pdfAction()
+    {
+        return array();
+    }
 }
