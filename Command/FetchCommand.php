fa14767fd36f810c2bb540f78039a5590a72f48a
Martijn van Maurik <martijn@mail.e-combined.nl>
2013-01-28 16:01:10 +0100

    Update

--- Command/FetchCommand.php
+++ Command/FetchCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mordred\Bundle\CMSBundle\Command;
+namespace NoM\Bundle\CMSBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
